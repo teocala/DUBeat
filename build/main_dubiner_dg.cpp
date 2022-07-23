de8ececda3cf1f07aa66c53ac8cfb62ac9eedc1d
Matteo Calafa <matteo.calafa@mail.polimi.it>
2022-07-23 16:11:17 +0200

    Added build with main file

---
+++ build/main_dubiner_dg.cpp
@@ -0,0 +1,52 @@
+/********************************************************************************
+  Copyright (C) 2019 - 2022 by the lifex authors.
+
+  This file is part of lifex.
+
+  lifex is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  lifex is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with lifex.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************/
+
+/**
+ * @file
+ *
+ * @author Federica Botta <federica.botta@mail.polimi.it>.
+ * @author Matteo Calafà <matteo.calafa@mail.polimi.it>.
+ */
+
+#include "lifex/core/init.hpp"
+
+#include "../models/heat_dg.hpp"
+#include "../models/laplace_dg.hpp"
+#include "../models/monodomain_dg.hpp"
+
+/// This file is an example of main execution script. The model below can be defined using the example models and adopting either DGFEM or Dubiner basis.
+
+int
+main(int argc, char **argv)
+{
+  lifex::lifex_init lifex_initializer(argc, argv, 1);
+
+  try
+    {
+      // Choose the model from the models folder and the basis from:
+      // Dubiner basis:    DUBValues<lifex::dim>
+      // FEM basis:        dealii::FE_SimplexDGP<lifex::dim>
+      lifex::examples::Laplace_DG<DUBValues<lifex::dim>> model;
+
+      model.main_run_generate();
+    }
+  LIFEX_CATCH_EXC();
+
+  return EXIT_SUCCESS;
+}
