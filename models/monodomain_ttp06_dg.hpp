f9e66f71515198fcc60ee7b8a9b01ca575a5c202
Federica Botta <federica.botta@mail.polimi.it>
2022-08-14 09:09:59 +0000

    now runs

--- models/monodomain_ttp06_dg.hpp
+++ models/monodomain_ttp06_dg.hpp
@@ -408,6 +408,13 @@ namespace lifex::examples
     /// Applied current for fixed time with ghost entries
     lifex::LinAlg::MPI::Vector I_app;
 
+    /// Override for declaration of additional parameters.
+    void
+    declare_parameters(lifex::ParamHandler &params) const override;
+
+    /// Override to parse additional parameters.
+    void
+    parse_parameters(lifex::ParamHandler &params) override;
 
     /// Override for the simulation run.
     void
@@ -425,6 +432,107 @@ namespace lifex::examples
     void
     assemble_system() override;
   };
+
+  template <class basis>
+  void
+  Monodomain_ttp06_DG<basis>::declare_parameters(lifex::ParamHandler &params) const
+  {
+    // Default parameters.
+    this->linear_solver.declare_parameters(params);
+    this->preconditioner.declare_parameters(params);
+    ionic_model->declare_parameters(params);
+
+    // Extra parameters.
+    params.enter_subsection("Mesh and space discretization");
+    {
+      params.declare_entry(
+        "Number of refinements",
+        "2",
+        dealii::Patterns::Integer(0),
+        "Number of global mesh refinement steps applied to initial grid.");
+      params.declare_entry("FE space degree",
+                           "1",
+                           dealii::Patterns::Integer(1),
+                           "Degree of the FE space.");
+    }
+    params.leave_subsection();
+
+    params.enter_subsection("Discontinuous Galerkin");
+    {
+      params.declare_entry(
+        "Penalty coefficient",
+        "-1",
+        dealii::Patterns::Double(-1, 1),
+        "Penalty coefficient in the Discontinuous Galerkin formulation.");
+      params.declare_entry(
+        "Stability coefficient",
+        "10",
+        dealii::Patterns::Double(0),
+        "Stabilization term in the Discontinuous Galerkin formulation.");
+    }
+    params.leave_subsection();
+
+    params.enter_subsection("Time solver");
+    {
+      params.declare_entry("Initial time",
+                           "0",
+                           dealii::Patterns::Double(0),
+                           "Initial time.");
+      params.declare_entry("Final time",
+                           "0.001",
+                           dealii::Patterns::Double(0),
+                           "Final time.");
+      params.declare_entry("Time step",
+                           "0.0001",
+                           dealii::Patterns::Double(0),
+                           "Time step.");
+      params.declare_entry("BDF order",
+                           "1",
+                           dealii::Patterns::Integer(1, 3),
+                           "BDF order: 1, 2, 3.");
+    }
+    params.leave_subsection();
+  }
+
+  template <class basis>
+  void
+  Monodomain_ttp06_DG<basis>::parse_parameters(lifex::ParamHandler &params)
+  {
+    // Parse input file.
+    params.parse();
+    // Read input parameters.
+    this->linear_solver.parse_parameters(params);
+    this->preconditioner.parse_parameters(params);
+    ionic_model->parse_parameters(params);
+
+    // Extra parameters.
+    params.enter_subsection("Mesh and space discretization");
+    this->prm_n_refinements = params.get_integer("Number of refinements");
+    this->prm_fe_degree     = params.get_integer("FE space degree");
+    params.leave_subsection();
+
+    params.enter_subsection("Discontinuous Galerkin");
+    this->prm_penalty_coeff = params.get_double("Penalty coefficient");
+    AssertThrow(this->prm_penalty_coeff == 1. || this->prm_penalty_coeff == 0. ||
+                  this->prm_penalty_coeff == -1.,
+                dealii::StandardExceptions::ExcMessage(
+                  "Penalty coefficient must be 1 (SIP method) or 0 (IIP method) "
+                  "or -1 (NIP method)."));
+
+    this->prm_stability_coeff = params.get_double("Stability coefficient");
+    params.leave_subsection();
+
+    params.enter_subsection("Time solver");
+    this->prm_time_init  = params.get_double("Initial time");
+    this->prm_time_final = params.get_double("Final time");
+    AssertThrow(this->prm_time_final > this->prm_time_init,
+                dealii::StandardExceptions::ExcMessage(
+                  "Final time must be greater than initial time."));
+
+    this->prm_time_step = params.get_double("Time step");
+    this->prm_bdf_order = params.get_integer("BDF order");
+    params.leave_subsection();
+  }
 
   template <class basis>
   void
@@ -628,7 +736,7 @@ namespace lifex::examples
             M *= Cm;
 
             cell_rhs = this->assemble->local_rhs(this->f_ex);
-
+            
             cell_rhs_ttp06 = this->assemble->local_ttp06(ionic_model, this->solution, this->solution_ext,
             solution_w, dof_indices);
             cell_rhs_ttp06 *= ChiM;
