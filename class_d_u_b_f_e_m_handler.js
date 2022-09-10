3b8599b84fc509ae89515914dd37c4795b578897
teocala <79268110+teocala@users.noreply.github.com>
2022-09-10 16:56:31 +0000

    Deploying to gh-pages from  @ 612873b1b7c96e023a83a34636f9e18c03b78fde 🚀

--- class_d_u_b_f_e_m_handler.js
+++ class_d_u_b_f_e_m_handler.js
@@ -6,9 +6,9 @@ var class_d_u_b_f_e_m_handler =
     [ "DUBFEMHandler", "class_d_u_b_f_e_m_handler.html#a0c6e9bc257b065bac4a63f7407217ff2", null ],
     [ "analytical_to_dubiner", "class_d_u_b_f_e_m_handler.html#ad2445c817b06a8cf73a0331b435fc48b", null ],
     [ "dubiner_to_fem", "class_d_u_b_f_e_m_handler.html#a091b63d3e1cce2e62c7fe28c44ec8cc5", null ],
-    [ "dubiner_to_fem", "class_d_u_b_f_e_m_handler.html#acb56ae0ec1eba70fff0a0f2a11a2123f", null ],
+    [ "dubiner_to_fem", "class_d_u_b_f_e_m_handler.html#ae285da1a9abe3051ec8348811b24b50d", null ],
     [ "fem_to_dubiner", "class_d_u_b_f_e_m_handler.html#a0be712f0ee7fcb92bc8e02a73dbb55ff", null ],
-    [ "fem_to_dubiner", "class_d_u_b_f_e_m_handler.html#ac1464cba85ce685dfc6143e98761d371", null ],
+    [ "fem_to_dubiner", "class_d_u_b_f_e_m_handler.html#a298c833e9714971f27eb43ca46c88c7c", null ],
     [ "dof_handler", "class_d_u_b_f_e_m_handler.html#a1c68731c0d98fb4969a7ab3468164992", null ],
     [ "n_quad_points", "class_d_u_b_f_e_m_handler.html#a044784832395e6242ae199d504d878c2", null ]
 ];
